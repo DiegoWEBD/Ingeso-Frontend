f7c518b49b053cdf1b8e77d090611ebe86ff7a1c
DiegoWEBD <diegomz.webdev@gmail.com>
2024-10-05 16:49:09 -0300

    correccion de errores

--- src/presentation/components/AppContainer.tsx
+++ src/presentation/components/AppContainer.tsx
@@ -6,7 +6,7 @@ interface AppContainerProps {
 
 const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
 	return (
-		<div className="mx-auto my-3 w-[90%] sm:w-[33rem] flex flex-col gap-3 text-[0.8rem] bg-blue-200">
+		<div className="mx-auto w-[90%] sm:w-[33rem] flex flex-col gap-3 text-[0.8rem]">
 			{children}
 		</div>
 	)
