98994369c8ff7d2266bec8130635561300e245c0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-19 14:27:09 -0300

    paginas sobrantes eliminadas y nombre de rutas actualizadas a español

--- src/presentation/components/dashboard/Dashboard.tsx
+++ src/presentation/components/dashboard/Dashboard.tsx
@@ -3,11 +3,9 @@ import DashboardTab from './DashboardTab'
 
 const Dashboard: React.FC = () => {
 	return (
-		<div className="bg-white py-5 px-3 flex flex-col gap-2 shadow-md shadow-gray-500/50 w-1/6">
-			<DashboardTab title="Inicio" to="/" />
-			<DashboardTab title="Fármacos" to="/drugs" />
-			<DashboardTab title="Tipos de fármacos" to="/drug_types" />
-			<DashboardTab title="Vías de administración" to="/administration_route" />
+		<div className='bg-white py-5 px-3 flex flex-col gap-2 shadow-md shadow-gray-500/50 w-1/6'>
+			<DashboardTab title='Inicio' to='/' />
+			<DashboardTab title='Fármacos' to='/farmacos' />
 		</div>
 	)
 }
