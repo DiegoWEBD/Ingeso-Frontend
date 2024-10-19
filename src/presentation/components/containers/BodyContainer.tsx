78fb829a37875b07868713d6da9a37cdfbfbc028
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-19 13:32:00 -0300

    arreglo de bugs

--- src/presentation/components/containers/BodyContainer.tsx
+++ src/presentation/components/containers/BodyContainer.tsx
@@ -6,7 +6,9 @@ type BodyContainerProps = {
 
 const BodyContainer: React.FC<BodyContainerProps> = ({ children }) => {
 	return (
-		<div className='flex flex-col mx-6 mt-12 sm:ml-5 gap-5'>{children}</div>
+		<div className='flex flex-col mx-6 mt-12 sm:ml-5 gap-5 w-full'>
+			{children}
+		</div>
 	)
 }
 
--- src/presentation/components/pages/drugs/drugs_list/DrugsList.tsx
+++ src/presentation/components/pages/drugs/drugs_list/DrugsList.tsx
@@ -8,11 +8,9 @@ type DrugsListProps = {
 const DrugsList: React.FC<DrugsListProps> = ({ drugNames }) => {
 	return (
 		<div
-			className='grid gap-4 overflow-y-auto max-h-[70vh] w-[80vw]'
+			className='grid gap-4 overflow-y-auto max-h-[70vh] py-2'
 			style={{
 				gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-				justifyItems: 'center', // Center items horizontally
-				alignItems: 'center', // Center items vertically
 			}}
 		>
 			{drugNames.map((drugName) => (
