2568f6fce594ec34d38900bb58f93923f621ddd0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-19 17:29:10 -0300

    agregado un método a la interfaz DrugRepository

--- src/domain/drug/DrugRepository.ts
+++ src/domain/drug/DrugRepository.ts
@@ -3,6 +3,7 @@ import Drug from './Drug'
 export default interface DrugRepository {
 	add: (drug: Drug) => Promise<void>
 	getAll: () => Promise<Array<Drug>>
+	getAllNames: () => Promise<Array<string>>
 	findByName: (name: string) => Promise<Drug | null>
 	update: (name: string, newValues: Drug) => Promise<void>
 	delete: (drug: Drug) => Promise<void>
