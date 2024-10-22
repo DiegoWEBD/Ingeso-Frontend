87d3924af8cd81dcd9a00f0ccf37eed1dfb68e29
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-21 22:34:48 -0300

    .

--- src/presentation/components/pages/drugs/drugs_list/DrugsList.tsx
+++ src/presentation/components/pages/drugs/drugs_list/DrugsList.tsx
@@ -7,7 +7,7 @@ type DrugsListProps = {
 
 const DrugsList: React.FC<DrugsListProps> = ({ drugNames }) => {
 	return (
-		<div className="grid grid-cols-5 gap-4">
+		<div className='grid grid-cols-5 gap-4'>
 			{drugNames.map((drugName) => (
 				<DrugsListItem key={drugName} drugName={drugName} />
 			))}
