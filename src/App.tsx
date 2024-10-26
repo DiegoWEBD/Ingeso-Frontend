bdd7a5a12bde4ede29c83ef0a1546017c36d344d
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-10-26 17:17:57 -0300

    validación token google, inicio de sesion, registro, custom token

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,35 @@ import HomePage from './presentation/components/pages/home/HomePage'
 import PageNotFound from './presentation/components/pages/not_found/PageNotFound'
 import DrugsPage from './presentation/components/pages/drugs/DrugsPage'
 import AddDrugPage from './presentation/components/pages/drugs/AddDrugPage'
-
+import LoginPage from './presentation/components/pages/login/LoginPage'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/farmacos',
-        element: <DrugsPage />,
-      },
-      {
-        path: '/farmacos/agregar',
-        element: <AddDrugPage />,
-      },
-      {
-        path: '*',
-        element: <PageNotFound />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <Root />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/login',
+				element: <LoginPage />,
+			},
+			{
+				path: '/farmacos',
+				element: <DrugsPage />,
+			},
+			{
+				path: '/farmacos/agregar',
+				element: <AddDrugPage />,
+			},
+			{
+				path: '*',
+				element: <PageNotFound />,
+			},
+		],
+	},
 ])
 
 const App: React.FC = () => {
