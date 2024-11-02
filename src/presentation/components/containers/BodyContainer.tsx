dd4e43e77ad6cab87ecb37e994e9f4fc060c8390
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-11-02 16:51:36 -0300

    cambio de posicion dhasboard en dispositivos mobiles

--- src/presentation/components/containers/BodyContainer.tsx
+++ src/presentation/components/containers/BodyContainer.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import { useLocation } from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 type BodyContainerProps = {
 	children: ReactNode
@@ -7,15 +8,16 @@ type BodyContainerProps = {
 
 const BodyContainer: React.FC<BodyContainerProps> = ({ children }) => {
 	const location = useLocation()
+	const accessToken = Cookies.get('access_token')
 
 	return (
 		<div
-			className='pt-[8rem] lg:pt-0 lg:pl-[25rem] '
+			className={accessToken ? 'pt-[8rem] lg:pt-0 lg:pl-[25rem]' : ''}
 			style={{
 				height: location.pathname === '/login' ? '100%' : '',
 			}}
 		>
-			<div className='flex flex-col p-5 gap-5 w-full bg-gray-100'>
+			<div className='flex flex-col p-5 gap-5 w-full bg-gray-100 h-full'>
 				{children}
 			</div>
 		</div>
