8ddd653c6a37a639ca21bcddbbfa7a309b6d44c0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-11-18 01:52:25 -0300

    correccion de bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './presentation/components/pages/Root'
-import HomePage from './presentation/components/pages/home/HomePage'
-import PageNotFound from './presentation/components/pages/not_found/PageNotFound'
-import DrugsPage from './presentation/components/pages/drugs/DrugsPage'
-import AddDrugPage from './presentation/components/pages/drugs/AddDrugPage'
-import LoginPage from './presentation/components/pages/login/LoginPage'
-import ProtectedComponent from './presentation/components/pages/ProtectedComponent'
-import ProfilePage from './presentation/components/pages/profile/ProfilePage'
 import { ErrorBoundary } from 'react-error-boundary'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ErrorFallback from './presentation/components/error/ErrorFallback'
+import ProtectedComponent from './presentation/components/pages/ProtectedComponent'
+import Root from './presentation/components/pages/Root'
+import DrugsPage from './presentation/components/pages/drugs/DrugsPage'
+import HomePage from './presentation/components/pages/home/HomePage'
+import LoginPage from './presentation/components/pages/login/LoginPage'
+import PageNotFound from './presentation/components/pages/not_found/PageNotFound'
+import ProfilePage from './presentation/components/pages/profile/ProfilePage'
 
 const router = createBrowserRouter([
 	{
@@ -32,10 +31,7 @@ const router = createBrowserRouter([
 				path: 'farmacos',
 				element: <ProtectedComponent children={<DrugsPage />} />,
 			},
-			{
-				path: 'farmacos/agregar',
-				element: <ProtectedComponent children={<AddDrugPage />} />,
-			},
+
 			{
 				path: 'perfil',
 				element: <ProtectedComponent children={<ProfilePage />} />,
