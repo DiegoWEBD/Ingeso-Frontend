eeda10f49d87abfa0af9922ab37f9e7b5ea01992
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-11-23 20:48:13 -0300

    corrección de bug con el token

--- src/presentation/components/containers/AppContainer.tsx
+++ src/presentation/components/containers/AppContainer.tsx
@@ -5,7 +5,6 @@ interface AppContainerProps {
 }
 
 const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
-	// h-[100vh] es temporal
 	return (
 		<div className='flex flex-col lg:flex-row h-[100vh] bg-secondary app-container'>
 			{children}
