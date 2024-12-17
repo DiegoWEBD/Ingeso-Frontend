2d34c508c761c890f4e4c1c24801f027db17c34d
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-12-17 17:28:18 -0300

    cambio url api

--- src/presentation/components/generic_components/notifications/InformativeNotification.tsx
+++ src/presentation/components/generic_components/notifications/InformativeNotification.tsx
@@ -1,17 +1,25 @@
 import { CheckCircle } from 'lucide-react'
 import React from 'react'
 import { useNotification } from './contexts/InformativeNotificationContext'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const InformativeNotification: React.FC = () => {
 	const { message, isVisible } = useNotification()
 
-	if (!isVisible) return null
-
 	return (
-		<div className="absolute bottom-2 right-2 bg-green-500 rounded-lg p-3 shadow-lg w-fit text-white flex gap-5 items-center">
-			<CheckCircle />
-			<p>{message}</p>
-		</div>
+		<AnimatePresence>
+			{isVisible && (
+				<motion.div
+					initial={{ opacity: 0, scale: 0.2 }}
+					animate={{ opacity: 1, scale: 1 }}
+					exit={{ opacity: 0, scale: 0.2 }}
+					className="fixed bottom-4 right-4 bg-green-500 rounded-lg p-3 shadow-lg w-fit text-white flex gap-5 items-center"
+				>
+					<CheckCircle />
+					<p>{message}</p>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	)
 }
 
