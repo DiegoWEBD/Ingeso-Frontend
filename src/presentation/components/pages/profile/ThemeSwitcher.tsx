f972cffbbdd37d98516976cc95ef3d64f78865f0
Diego Maldonado Zamorano <diegomz.webdev@gmail.com>
2024-12-20 18:17:20 -0300

    mejora estilos de selector de temas

--- src/presentation/components/pages/profile/ThemeSwitcher.tsx
+++ src/presentation/components/pages/profile/ThemeSwitcher.tsx
@@ -4,13 +4,13 @@ import StudentComponent from '../../protected/StudentComponent'
 
 const ThemeSwitcher: React.FC = () => {
 	return (
-		<div className="bg-card shadow-md flex flex-col gap-4 w-full w-[25rem] rounded-md p-6 break-words">
+		<div className="bg-card shadow-md flex flex-col gap-4 w-[18rem] sm:w-[25rem] rounded-md p-6 break-words">
 			<div className="text-xl font-bold text-primary-intense flex gap-2 items-center pb-4">
 				<PaintBucket className="text-primary" />
 				<p>Tema</p>
 			</div>
 
-			<div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 				<ThemeOption themeName="Verde" themeId="" />
 				<ThemeOption themeName="Azul" themeId="blue" />
 
